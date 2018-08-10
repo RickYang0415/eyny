71e2c24f524b4a6659598227b1cd4237b897717c
Rick Yang <rickming0415@hotmail.com>
2018-08-10 13:52:59 +0800

    Update eyny.js

--- eyny.js
+++ eyny.js
@@ -1,12 +1,12 @@
 ﻿javascript: (
     function () {
-        var strFla = document.getElementById("mediaplayer").innerHTML;
+        var strFla = document.getElementById("mediaplayer");
         var divLink = document.createElement("a");
         if (strFla == undefined) {
             divLink.innerHTML = 'Parse fail!!';
         }
         else {
-            strFla = unescape(strFla.split('src')[1].split("=")[1]).split(" ")[0];
+            strFla = strFla.childNodes[0].getAttribute("src");
             divLink.href = strFla.replace(/\"/g, "");
             divLink.download = "video.mp4";
             divLink.setAttribute("style", "z-index:9999;position:fixed;top:0;right:0;font-size:40px;color:red");
